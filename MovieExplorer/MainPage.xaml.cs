6e1062b16804b4eeb0b970daec1b0473dc0a3a93
MAYQUADROS <102834585+MAYQUADROS@users.noreply.github.com>
2025-11-15 19:48:51 +0000

    toggle theme button added

--- MovieExplorer/MainPage.xaml.cs
+++ MovieExplorer/MainPage.xaml.cs
@@ -1,14 +1,44 @@
-﻿namespace MovieExplorer
+﻿using Microsoft.Maui.Graphics.Text;
+using Microsoft.UI.Xaml.Documents;
+using Windows.UI.Text;
+
+namespace MovieExplorer
 {
     public partial class MainPage : ContentPage
     {
-        
+        private bool lightTheme;
 
         public MainPage()
         {
             InitializeComponent();
+            lightTheme = Preferences.Default.Get("LightTheme", true);
         }
 
-    
+        private void ToggleThemeButton_Clicked(object sender, EventArgs e)
+        {
+            lightTheme = !lightTheme;
+            applyTheme();
+            Preferences.Default.Set("LightTheme", lightTheme);
+        }
+
+
+        private void applyTheme()
+        {
+            if (lightTheme)
+            {
+                Resources["PageBackgroundColor"] = Resources["LightBackgroundColor"];
+                ToggleThemeButton.TextColor = Colors.Black;
+                ToggleThemeButton.BackgroundColor = Colors.BlueViolet;
+                
+
+            }
+            else
+            {
+                Resources["PageBackgroundColor"] = Resources["DarkBackgroundColor"];
+                ToggleThemeButton.TextColor = Colors.LightGray;
+                ToggleThemeButton.BackgroundColor = Colors.Teal;
+            }
+        }
     }
 }
+   
--- MovieExplorer/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/MovieExplorer.AssemblyInfo.cs
+++ MovieExplorer/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/MovieExplorer.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MovieExplorer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+c5fbdb54b0a6387b970cbd2cbdf520522a728c74")]
 [assembly: System.Reflection.AssemblyProductAttribute("MovieExplorer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MovieExplorer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
