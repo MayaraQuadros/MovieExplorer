349720d33cababa6c35e3cfc6e5ed586245b3c76
MAYQUADROS <102834585+MAYQUADROS@users.noreply.github.com>
2025-11-15 17:34:24 +0000

    starting

---
+++ MovieExplorer/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace MovieExplorer
+{
+    public partial class MainPage : ContentPage
+    {
+        
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+    
+    }
+}
